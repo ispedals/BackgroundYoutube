2383b03fc626098c9fbda19e960ee5a5199a6f8b
ispedals <ispedals@users.noreply.github.com>
2015-07-26 16:55:28 -0400

    Remove unnecessary specificiation of DASH mediatype
    The library can determine if the url is a DASH url, so remove the copy-pasted
    sections.

--- app/src/main/java/is/pedals/backgroundyoutube/MediaPlayerService.java
+++ app/src/main/java/is/pedals/backgroundyoutube/MediaPlayerService.java
@@ -4,14 +4,9 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.AudioManager;
-import android.net.Uri;
-import android.os.PowerManager;
 import android.support.annotation.Nullable;
 
-import com.devbrackets.android.exomedia.EMAudioPlayer;
 import com.devbrackets.android.exomedia.service.EMPlaylistService;
-import com.devbrackets.android.exomedia.util.MediaUtil;
 
 //copied from exomediademo/service/AudioService.java
 public class MediaPlayerService extends EMPlaylistService<MediaItem, PlaylistManager> {
@@ -23,47 +18,6 @@ public class MediaPlayerService extends EMPlaylistService<MediaItem, PlaylistMan
     @Override
     protected String getAppName() {
         return getResources().getString(R.string.app_name);
-    }
-
-    /*
-    this method is private in EMPlaylistService, be we need to call this in our overridden playAudioItem() implementation,
-    so we just copied it
-     */
-    void initializeAudioPlayer() {
-        if (audioPlayer != null) {
-            audioPlayer.reset();
-            return;
-        }
-
-        audioPlayer = new EMAudioPlayer(getApplicationContext());
-        audioPlayer.setBus(getBus());
-        audioPlayer.startProgressPoll(this);
-        audioPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-
-        audioPlayer.setOnPreparedListener(audioListener);
-        audioPlayer.setOnCompletionListener(audioListener);
-        audioPlayer.setOnErrorListener(audioListener);
-    }
-
-    @Override
-    /*
-    we need to
-     */
-    protected void playAudioItem() {
-        stopVideoPlayback();
-        initializeAudioPlayer();
-        audioFocusHelper.requestFocus();
-
-        audioPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        audioPlayer.setDataSource(this, Uri.parse(currentPlaylistItem.getMediaUrl()), MediaUtil.MediaType.DASH);
-
-        setMediaState(MediaState.PREPARING);
-        setupAsForeground();
-
-        audioPlayer.prepareAsync();
-
-        wifiLock.acquire();
-
     }
 
     @Override
